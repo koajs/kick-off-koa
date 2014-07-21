9d785bda5a696cd9749eec1cfc80ac5f88d68d95
hemanth.hm <hemanth.hm@gmail.com>
2014-07-21 18:45:59 +0530

    kick-off-koa
    It must be `kick-off-koa` or should we change the `bin` in the `package.json` to `kickoffkoa` ?

--- kick-off-koa.js
+++ kick-off-koa.js
@@ -3,7 +3,7 @@ var Workshopper = require('workshopper');
 var path = require('path');
 var credits = require('./credits');
 
-var name = 'kickoffkoa';
+var name = 'kick-off-koa';
 var title = 'KICK OFF KOA';
 var subtitle = '\x1b[23mSelect an exercise and hit \x1b[3mEnter\x1b[23m to begin';
 
