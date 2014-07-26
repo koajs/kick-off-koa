6ba8b5b82389ef3c7110faa339e1f792748d3e0a
hemanth.hm <hemanth.hm@gmail.com>
2014-07-26 08:25:14 +0530

    Thunkified fs.stat.

--- exercises/stream_file/solution/solution.js
+++ exercises/stream_file/solution/solution.js
@@ -6,13 +6,23 @@ var extname = path.extname;
 
 app.use(function * () {
   var path = __dirname + this.path;
-  if (fs.lstatSync(path).isFile()) {
+  var fstat = yield stat(path);
+
+  if (fstat.isFile()) {
     this.type = extname(path);
     this.body = fs.createReadStream(path);
   } else {
     this.satus = 404;
     this.body = "File not found.";
   }
+
+  function stat(file) {
+    return function(done) {
+      fs.stat(file, done);
+    };
+  }
 });
+
+if (!module.parent) app.listen(3000);
 
 app.listen(process.argv[2]);
