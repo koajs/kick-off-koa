efff0cefbcab81fcb476792294b76b22fdf617e9
Eole <Aessi@users.noreply.github.com>
2014-08-04 13:19:34 +0200

    Small typo fix, it's "request" and not "requset".

--- exercises/error_handling/exercise.js
+++ exercises/error_handling/exercise.js
@@ -3,11 +3,11 @@ var exercise = require('../../exercise');
 
 module.exports = exercise
 .push('/', function (data, res, stream) {
-  stream.write('responds ' + data.toString() + ' when requset /\n');
+  stream.write('responds ' + data.toString() + ' when request /\n');
   stream.write('response status is ' + res.statusCode + ' when request /\n');
 })
 .push('/error', function (data, res, stream) {
-  stream.write('responds ' + data.toString() + ' when requset /error\n');
+  stream.write('responds ' + data.toString() + ' when request /error\n');
   stream.write('response status is ' + res.statusCode + ' when request /error\n');
 })
 .generate();
