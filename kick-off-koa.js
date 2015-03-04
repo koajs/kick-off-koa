6fd0dc638bc396d19479e513ef965a1283cf4321
Christophe Porteneuve <tdd@tddsworld.com>
2015-03-04 17:14:04 +0100

    g11n upgrade based on latest workshopper infra + core FR L10n

--- kick-off-koa.js
+++ kick-off-koa.js
@@ -4,9 +4,6 @@ var path = require('path');
 var credits = require('./credits');
 
 var name = 'kick-off-koa';
-var title = 'KICK OFF KOA';
-var subtitle = '\x1b[23mSelect an exercise and hit \x1b[3mEnter\x1b[23m to begin';
-
 
 function fpath (f) {
   return path.join(__dirname, f);
@@ -14,14 +11,11 @@ function fpath (f) {
 
 Workshopper({
     name : name,
-    title : title,
-    subtitle : subtitle,
-    exerciseDir : fpath('./exercises/'),
     appDir : __dirname,
-    helpFile : fpath('help.txt'),
+    languages : ['en', 'fr'],
+    helpFile : fpath('./i18n/help/{lang}.txt'),
     menuItems : [{
         name : 'credits',
         handler : credits
-    }],
-    menuOptions : {}
+    }]
 });
